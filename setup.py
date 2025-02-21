2afe899a47bfa4b4898279734365b53d76a47900
Chris Withers <chris@simplistix.co.uk>
2025-02-21 09:23:45 +0000

    Officially support Python 3.12 and 3.13

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     packages=find_packages(exclude=['tests']),
     python_requires=">=3.6",
